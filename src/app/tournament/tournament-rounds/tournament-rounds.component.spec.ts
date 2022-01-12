376eba24560d57226d0d9c03a5de8a7d32903ac0
Grzegorz Malewicz <grzegorz.malewicz@gmail.com>
2022-01-12 21:49:22 +0100

    removed duplications

--- src/app/tournament/tournament-rounds/tournament-rounds.component.spec.ts
+++ src/app/tournament/tournament-rounds/tournament-rounds.component.spec.ts
@@ -6,7 +6,6 @@ import { HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faPlusCircle } from '@fortawesome/free-solid-svg-icons/faPlusCircle';
-import { log } from 'console';
 import { MimicBackendTournamentInterceptor } from '../_helpers/MimicBackendTournamentInterceptor';
 
 import { TournamentHttpService } from '../_services';
@@ -34,33 +33,7 @@ describe('TournamentRoundsComponent', () => {
     .compileComponents();
   }));
 
-  it('should create', () => {
-    fixture = TestBed.createComponent(TournamentRoundsComponent);
-    history.pushState({test: {}}, '');
-    component = fixture.componentInstance;
-    component.faPlusCircle = faPlusCircle;
-    component.tournament = {id: 1, name: 'test', startDate: '2020/10/10', endDate: '2020/10/10'};
-    component.rounds = [{course: {name: 'Lisia Polana', par: 72}, roundDate: '10/10/2020',
-    matchPlay: false, player: [{nick: 'test', roundDetails: {whs: 10, cr: 68, sr: 133, teeId: 0, teeType: teeTypes.TEE_TYPE_18}}],
-    scoreCard: [{hole: 1, stroke: 1, pats: 0}]}];
-    fixture.detectChanges();
-    expect(component).toBeTruthy();
-  });
-
-  it('should create and display data', () => {
-    fixture = TestBed.createComponent(TournamentRoundsComponent);
-    history.pushState({data: {tournament: {id: 1, name: 'test', startDate: '10/10/2020', endDate: '10/10/2020', player: {id: 1}}}}, '');
-    component = fixture.componentInstance;
-    component.faPlusCircle = faPlusCircle;
-    component.tournament = {id: 1, name: 'test', startDate: '2020/10/10', endDate: '2020/10/10'};
-    component.rounds = [{course: {name: 'Lisia Polana', par: 72}, roundDate: '10/10/2020',
-    matchPlay: false, player: [{nick: 'test', roundDetails: {whs: 10, cr: 68, sr: 133, teeId: 0, teeType: teeTypes.TEE_TYPE_18}}],
-    scoreCard: [{hole: 1, stroke: 1, pats: 0}]}];
-    fixture.detectChanges();
-    expect(component).toBeTruthy();
-  });
-
-  it('should add round', () => {
+  beforeEach(() => {
     fixture = TestBed.createComponent(TournamentRoundsComponent);
     history.pushState({data: {tournament: {id: 1, name: 'test', startDate: '10/10/2020', endDate: '10/10/2020', player: {id: 1}}}}, '');
     component = fixture.componentInstance;
@@ -69,8 +42,22 @@ describe('TournamentRoundsComponent', () => {
     component.rounds = [{course: {name: 'Lisia Polana', par: 72}, roundDate: '10/10/2020',
     matchPlay: false, player: [{nick: 'test', roundDetails: {whs: 10, cr: 68, sr: 133, teeId: 0, teeType: teeTypes.TEE_TYPE_18}}],
     scoreCard: [{hole: 1, stroke: 1, pats: 0}], id: 1}];
-    component.addRound(component.rounds[0]);
+  });
 
+
+  it('should create', () => {
+    history.pushState({test: {}}, '');
+    fixture.detectChanges();
+    expect(component).toBeTruthy();
+  });
+
+  it('should create and display data', () => {
+    fixture.detectChanges();
+    expect(component).toBeTruthy();
+  });
+
+  it('should add round', () => {
+    component.addRound(component.rounds[0]);
     expect(component.rounds.length).toEqual(0);
   });
 
@@ -79,3 +66,7 @@ describe('TournamentRoundsComponent', () => {
     TestBed.resetTestingModule();
   });
 });
+function prepareData() {
+  throw new Error('Function not implemented.');
+}
+
