3f828aa324af7c0c91c3f4e19e0d3088b679cdbc
Grzegorz Malewicz <grzegorz.malewicz@gmail.com>
2022-04-12 15:47:51 +0200

    update

--- src/app/tournament/tournament-rounds/tournament-rounds.component.ts
+++ src/app/tournament/tournament-rounds/tournament-rounds.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Round } from '@/_models';
 import { AlertService, AuthenticationService } from '@/_services';
 import { IconDefinition } from '@fortawesome/free-solid-svg-icons';
-import { faPlusCircle } from '@fortawesome/free-solid-svg-icons/faPlusCircle';
+import { faPlusCircle } from '@fortawesome/free-solid-svg-icons';
 import { Router } from '@angular/router';
 import { TournamentHttpService } from '../_services';
 import { tap } from 'rxjs/operators';
