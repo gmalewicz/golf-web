3561b016d1a87519d338f9999de0f88b7888d24a
Grzegorz Malewicz <grzegorz.malewicz@gmail.com>
2023-10-08 18:01:33 +0200

    bug fixes

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -2,8 +2,6 @@ import { Component, Inject, LOCALE_ID, OnInit} from '@angular/core';
 import { Player } from '@/_models';
 import { AuthenticationService, AlertService } from '@/_services';
 import { Router } from '@angular/router';
-import { faCog, IconDefinition } from '@fortawesome/free-solid-svg-icons';
-
 
 @Component({
   selector: 'app-navigation',
@@ -11,7 +9,6 @@ import { faCog, IconDefinition } from '@fortawesome/free-solid-svg-icons';
 })
 export class NavigationComponent implements OnInit {
 
-  faCog: IconDefinition;
   display: boolean;
   currentPlayer: Player;
 
@@ -27,7 +24,6 @@ export class NavigationComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.faCog = faCog;
     this.display = false;
     this.authenticationService.currentPlayer.subscribe(x => this.currentPlayer = x);
   }
