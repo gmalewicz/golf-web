0786054805a56e417f345fc6e130e1eaadd895f6
Grzegorz Malewicz <grzegorz.malewicz@gmail.com>
2025-01-21 13:51:32 +0100

    duplication fixed

--- src/app/cycles/cycle-results-stroke-play/cycle-results-stroke-play.component.ts
+++ src/app/cycles/cycle-results-stroke-play/cycle-results-stroke-play.component.ts
@@ -4,6 +4,7 @@ import { ChangeDetectionStrategy, Component, OnChanges, OnInit, input } from '@a
 import { Router } from '@angular/router';
 import { Cycle } from '../_models/cycle';
 import { CycleResult } from '../_models/cycleResult';
+import { CycleResultsBase } from '../base/cycle-results-base';
 
 
 @Component({
@@ -12,47 +13,27 @@ import { CycleResult } from '../_models/cycleResult';
     changeDetection: ChangeDetectionStrategy.OnPush,
     imports: []
 })
-export class CycleResultsStrokePlayComponent implements OnInit, OnChanges {
+export class CycleResultsStrokePlayComponent extends CycleResultsBase implements OnInit, OnChanges {
 
   cycle = input.required<Cycle>();
   cycleResults = input.required<CycleResult[]>();
   strokePlayResults = [];
   cycleTournaments = input.required<CycleTournament[]>();
-  display: boolean;
-  rounds: number[];
-  names: number[];
-
+  
   constructor(public authenticationService: AuthenticationService,
-              private readonly router: Router) { }
+              protected readonly router: Router) { 
+                
+    super(authenticationService, router);
+  }
 
   ngOnInit(): void {
-    if (this.authenticationService.currentPlayerValue === null) {
-      this.authenticationService.logout();
-      this.router.navigate(['/login']).catch(error => console.log(error));
-    } else {
-
-      
-      this.display = true;
-    }
+    this.init();
   }
 
   ngOnChanges(): void {
-    this.rounds = [];
-    this.names = [];
-    if (this.cycleTournaments().length > 0) {
-
-      let offset = 0;
-      this.cycleTournaments().map(v => v.rounds).forEach((n, idx) => {
-
-        for (let i = 0; i < n; i++) {
-          this.rounds.push(offset + i);
-          this.names.push(idx);
-        }
-        offset += 4;
-      });
-    }
-
     
+    this.changes();
+
     this.cycleResults().forEach(v => {v.played =  v.r.reduce((a, b) => a + (b > 0 ? 1 : 0), 0)});
     const bestRoundsAchieved = this.cycleResults().filter(v => v.series === 2 && v.played >= this.cycle().bestRounds);
     const bestRoundsNotAchieved = this.cycleResults().filter(v => v.series === 2 && v.played < this.cycle().bestRounds);
@@ -63,11 +44,5 @@ export class CycleResultsStrokePlayComponent implements OnInit, OnChanges {
     bestRoundsNotAchieved.sort((a, b) =>  b.played - a.played || a.cycleResult - b.cycleResult);
 
     this.strokePlayResults = bestRoundsAchieved.concat(bestRoundsNotAchieved);
-  }
-
-
-  // helper function to provide verious arrays for html
-  counter(i: number) {
-    return [...Array(i).keys()];
   }
 }
