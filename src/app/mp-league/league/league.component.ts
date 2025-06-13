f152c20986e105fc3206a1c3f9b57a0b55384fc2
Grzegorz Malewicz <grzegorz.malewicz@gmail.com>
2025-06-13 12:00:03 +0200

    initial version

--- src/app/mp-league/league/league.component.ts
+++ src/app/mp-league/league/league.component.ts
@@ -131,12 +131,8 @@ export class LeagueComponent  implements OnInit {
 
     this.navigationService.matches.update(matches => { matches.forEach(match => {
         // update only if winnerNick is undefined (when it is read from database)
-        if (match.winnerNick === undefined) {
-          match.winnerNick = this.navigationService.players().find(player => player.playerId === match.winnerId).nick;
-        }
-        if (match.looserNick === undefined) {
-          match.looserNick = this.navigationService.players().find(player => player.playerId === match.looserId).nick;
-        }
+        match.winnerNick = match.winnerNick ?? this.navigationService.players().find(player => player.playerId === match.winnerId).nick;
+        match.looserNick = match.looserNick ?? this.navigationService.players().find(player => player.playerId === match.looserId).nick;
       })
       return matches;
     });
